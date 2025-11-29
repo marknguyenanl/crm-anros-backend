a83bd207c7119a72ff1929cbee78a0789d977f53
mark <marknguyenanl@gmail.com>
2025-11-29 09:04:11 +0700

    chore: password hashed

--- app/Models/User.php
+++ app/Models/User.php
@@ -28,6 +28,7 @@ class User extends Authenticatable implements JWTSubject
 
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'password' => 'hashed',
     ];
 
     protected $hidden = [
